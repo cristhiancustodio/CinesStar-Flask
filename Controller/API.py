8458e34b670acdd9a77d5b1b1a4dc40d879c6513
Cristhian Custodio <cryisthian_06@hotmail.com>
2024-07-28 14:09:14 -0500

    qq

--- Controller/API.py
+++ Controller/API.py
@@ -26,7 +26,7 @@ class API:
     #def listarRutas(self):
     @api_blue.route("/lista-palabras")
     def lista_palabras():
-        return "prueba1"
+        return "comentario de laptop"
         return API.listaPalabrasClaves()
     
     @api_blue.route("/API/", methods=['POST'])
